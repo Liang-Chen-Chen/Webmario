9aa2f425b557dd9f505ae5e4031e0e294bce5225
112034071 <talo930810@gmail.com>
2026-05-21 23:51:54 +0800

    last version

--- assets/scripts/GameOverView.ts
+++ assets/scripts/GameOverView.ts
@@ -6,17 +6,10 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class GameOverView extends cc.Component {
 
-    @property(cc.Label) finalScoreLabel: cc.Label = null;
-    @property(cc.Label) finalCoinLabel: cc.Label = null;
+    
 
     start() {
-        // 從全域資料庫把剛才 GameManager 存進來的資料拿出來顯示
-        if (this.finalScoreLabel) {
-            this.finalScoreLabel.string = "FINAL SCORE: " + GameData.score;
-        }
-        if (this.finalCoinLabel) {
-            this.finalCoinLabel.string = "TOTAL COINS: X" + GameData.coinCount.toString().padStart(3, '0');
-        }
+       
     }
 
     // 「重新開始」按鈕點擊事件
